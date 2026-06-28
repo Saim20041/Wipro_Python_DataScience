a83a885bfa23209f18d28018a9e611103d9b52d0
Saim20041 <saimk2003@gmail.com>
2026-06-28 21:05:12 +0530

    Delete miniProject1.py

--- miniProject1.py
+++
@@ -1,8 +0,0 @@
-distance = int(input("How far would you like to travel in miles? "))
-
-if distance < 3:
-    print("I suggest Bicycle to your destination")
-elif distance < 300:
-    print("I suggest Motor-Cycle to your destination")
-else:
-    print("I suggest Super-Car to your destination")
